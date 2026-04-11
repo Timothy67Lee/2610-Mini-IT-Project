22f72ad0d2f57411c501fac7d4c4431e84c0b8bb
ROWEN SEBASTIAN JAMESRAJA <rowensebastian97@gmail.com>
2026-04-11 23:19:28 +0800

    Added cards to index but CSS picture error

--- homepage/resources/views/index.blade.php
+++ homepage/resources/views/index.blade.php
@@ -8,22 +8,46 @@
 </head>
 <body>
 
-        <h2>Explore Our Clubs</h2>
-        <div class="club-grid">
+    <h2>Explore Our Clubs</h2>
+    <div class="club-grid">
 
             <div class="club-card">
-                <img src="{{ asset('images/picture 1.jpg') }}" alt="'Drama Club" style="width:100%; height:auto;">
+                <img src="{{ asset('images/picture2.jpg') }}" alt="'Drama Club">
                 <h3>Drama Club</h3>
                 <p>Explore your creativity through acting and stage performance.</p>
             </div>
 
             <div class="club-card">
-                <img src="{{ asset('images/picture 2.jpg') }}" alt="'Chess Club" style="width:100%; height:auto;">
+                <img src="{{ asset('images/picture1.jpg') }}" alt="'Chess Club">
                 <h3>Chess Club</h3>
                 <p>Sharpen your mind with weekly chess matches and tournaments.</p>
             </div>
-        </div>
-    </main>
+
+            <div class="club-card">
+                <img src="{{ asset('images/picture3.jpg') }}" alt="'Music Club">
+                <h3>Music Club</h3>
+                <p>Bring out the musical talent in you in our club.</p>
+            </div>
+
+            <div class="club-card">
+                <img src="{{ asset('images/picture4.jpg') }}" alt="'Football Club"> 
+                <h3>Football Club</h3>
+                <p>You got Ball knowledge? Join the Foootball Club.</p>
+            </div>
+
+            <div class="club-card">
+                <img src="{{ asset('images/picture5.jpg') }}" alt="'Badminton Club">
+                <h3>Badminton Club</h3>
+                <p>Lee Chong Wei academy.</p>
+            </div>
+
+            <div class="club-card">
+                <img src="{{ asset('images/picture6.jpg') }}" alt="'Basketball Club"> 
+                <h3>BoBasketball Club</h3>
+                <p>Practice drills and play competitive games with us.</p>
+            </div>
+
+         </div>
 
     <footer>
         <p> 2026 UNIVERSITY CLUBS </p>
