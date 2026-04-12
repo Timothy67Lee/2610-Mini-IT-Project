6027f2b940912ab01d81996f8525d06152f57812
ROWEN SEBASTIAN JAMESRAJA <rowensebastian97@gmail.com>
2026-04-12 12:50:55 +0800

    CSS Clubd card pictures fixed

--- homepage/resources/views/index.blade.php
+++ homepage/resources/views/index.blade.php
@@ -12,38 +12,38 @@
     <div class="club-grid">
 
             <div class="club-card">
-                <img src="{{ asset('images/picture2.jpg') }}" alt="'Drama Club">
+                <img src="{{ asset('images/picture2.jpg') }}" alt="Drama Club">
                 <h3>Drama Club</h3>
                 <p>Explore your creativity through acting and stage performance.</p>
             </div>
 
             <div class="club-card">
-                <img src="{{ asset('images/picture1.jpg') }}" alt="'Chess Club">
+                <img src="{{ asset('images/picture1.jpg') }}" alt="Chess Club">
                 <h3>Chess Club</h3>
                 <p>Sharpen your mind with weekly chess matches and tournaments.</p>
             </div>
 
             <div class="club-card">
-                <img src="{{ asset('images/picture3.jpg') }}" alt="'Music Club">
+                <img src="{{ asset('images/picture3.jpg') }}" alt="Music Club">
                 <h3>Music Club</h3>
                 <p>Bring out the musical talent in you in our club.</p>
             </div>
 
             <div class="club-card">
-                <img src="{{ asset('images/picture4.jpg') }}" alt="'Football Club"> 
+                <img src="{{ asset('images/picture4.jpg') }}" alt="Football Club"> 
                 <h3>Football Club</h3>
-                <p>You got Ball knowledge? Join the Foootball Club.</p>
+                <p>You got Ball knowledge? Join the Football Club.</p>
             </div>
 
             <div class="club-card">
-                <img src="{{ asset('images/picture5.jpg') }}" alt="'Badminton Club">
+                <img src="{{ asset('images/picture5.jpg') }}" alt="Badminton Club">
                 <h3>Badminton Club</h3>
                 <p>Lee Chong Wei academy.</p>
             </div>
 
             <div class="club-card">
-                <img src="{{ asset('images/picture6.jpg') }}" alt="'Basketball Club"> 
-                <h3>BoBasketball Club</h3>
+                <img src="{{ asset('images/picture6.jpg') }}" alt="Basketball Club"> 
+                <h3>Basketball Club</h3>
                 <p>Practice drills and play competitive games with us.</p>
             </div>
 
