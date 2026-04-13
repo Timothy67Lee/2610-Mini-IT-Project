e055928afd803257fe6212fbc412da4e9713756e
Timothy Lee Roberts <timlee240907@gmail.com>
2026-04-13 14:57:12 +0800

    Began designing log in page

--- CSRW-Website/resources/views/log-in.blade.php
+++ CSRW-Website/resources/views/log-in.blade.php
@@ -3,9 +3,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log In</title>
+    <link rel="stylesheet" href="{{ asset('css/log-in.css') }}">
 </head>
 <body>
-    <form action="" method="post">
+    <form action="log-in.blade.php" method="GET">
         <label for="Email">Email</label><br>
         <input type="text" name="Email"><br>
         <label for="Password">Password</label><br>
@@ -14,3 +15,7 @@
     </form>
 </body>
 </html>
+
+<? php
+    echo $_GET["Email"];
+?>
