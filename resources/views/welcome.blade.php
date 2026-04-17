229323331e9ad2d6fb74c7d7135098066e7b56e5
loozh <loozhiheng@gmail.com>
2026-04-17 23:22:59 +0800

    blade links obtained

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,5 +10,21 @@
     <a href="" class="btn">
         Find Clubs or Society!
     </a>
+
+    <nav>
+        @if (Route::has('login'))
+    <div class="fixed top-0 right-0 p-6 text-right z-10">
+        @auth
+            <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900">Dashboard</a>
+        @else
+            <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900">Log in</a>
+
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900">Register</a>
+            @endif
+        @endauth
+    </div>
+@endif
+<nav>
 </body>
 </html>
