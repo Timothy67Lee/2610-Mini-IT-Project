ae49d8b24778c756d2d14d0925d5f8a0c86a308e
Timothy Lee Roberts <timlee240907@gmail.com>
2026-04-18 17:44:35 +0800

    Working on the navigation page now

--- CSRW-Website/routes/web.php
+++ CSRW-Website/routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
-Route::get('/', function () {
-    return view('log-in');
-});
 
 Route::get('/log-in.blade.php', function () {
     return view('log-in');
+});
+
+Route::get('/', function () {
+    return view('navigation');
 });
 
