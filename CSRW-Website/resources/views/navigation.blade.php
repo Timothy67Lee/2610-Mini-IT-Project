a3e173e2604adc2041728b9245f0c0fa37f83234
Timothy Lee Roberts <timlee240907@gmail.com>
2026-04-18 18:55:33 +0800

    Basic design of navigation page

---
+++ CSRW-Website/resources/views/navigation.blade.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/navigation-page.css') }}">
+    <title>Navigation</title>
+</head>
+<body>
+    <div class="container">
+        <img src="images/1.jpg">
+        <img src="images/2.jpg">
+        <img src="images/3.png">
+        <img src="images/4.png">
+        <img src="images/5.png">
+        <img src="images/6.jpg">
+        <img src="images/7.jpg">
+        <img src="images/8.jpg">
+        <img src="images/9.jpg">
+
+    </div>
+    
+</body>
+</html>
