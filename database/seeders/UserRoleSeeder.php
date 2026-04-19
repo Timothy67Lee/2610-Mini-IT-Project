6c9befceaf737e5b29f0ed90c49c5bf57487aa82
loozh <loozhiheng@gmail.com>
2026-04-19 23:21:44 +0800

    blade change to dark mode ??

--- database/seeders/UserRoleSeeder.php
+++ database/seeders/UserRoleSeeder.php
@@ -1,18 +1,24 @@
 <?php
+
 namespace Database\Seeders;
 
-use App\Models\UserRole;
-use Illuminate\Database\Seeder;
+use App\Models\User;                     
 use App\Enums\UserRole;
+use App\Enums\UserStatus;
+use App\Enums\UserVerification;
+use Illuminate\Database\Seeder;
 
-class UserRoleSeeder extends Seeder{
-
+class UserRoleSeeder extends Seeder
+{
     public function run(): void
     {
         User::create([
-            'name'   => 'Admin User',
-            'email'  => 'admin@example.com',
-            'role'   => UserRole::ADMIN,
+            'name'         => 'Admin User',
+            'email'        => 'admin@example.com',
+            'password'     => bcrypt('password'),
+            'role'         => UserRole::ADMIN,
+            'status'       => UserStatus::ACTIVE,
+            'verification' => UserVerification::VERIFIED,
         ]);
     }
 }
