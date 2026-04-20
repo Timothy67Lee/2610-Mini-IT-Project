f5ce40e0b89cefbf01c493cbf6970795fe983133
ROWEN SEBASTIAN JAMESRAJA <rowensebastian97@gmail.com>
2026-04-20 15:12:37 +0800

    Homepage branch: added create clubs/posts migrations

--- homepage/database/migrations/2026_04_14_124109_create_clubs_table.php
+++ homepage/database/migrations/2026_04_14_124109_create_clubs_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Do the migrations.
      */
     public function up(): void
     {
