800e63749dcce6b9a7d1119e943a4a5f32fba03f
Timothy Lee Roberts <timlee240907@gmail.com>
2026-04-26 18:49:28 +0800

    Rough idea of top-nav version 2.0

--- CSRW-Website/routes/web.php
+++ CSRW-Website/routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/navigation', function(){
     $clubs = [
         ["name" => "MMUsic Club", "profile_picture" => "images/1.jpg", "id" => "1", "category" => "Arts Clubs"],
         ["name" => "Badminton Club", "profile_picture" => "images/2.jpg", "id" => "2", "category" => "Recreational / Physical Activities Clubs"],
@@ -24,6 +28,14 @@ Route::get('/clubs/{name}', function($name){
     return view('clubs.show', ["name" => $name]);
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
+require __DIR__.'/auth.php';
