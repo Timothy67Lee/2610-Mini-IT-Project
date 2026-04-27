679b002a9f7920c9613888530b404fb29fb67e6d
loozh <loozhiheng@gmail.com>
2026-04-27 16:02:45 +0800

    syntax fix

--- app/Enums/ClubCategory.php
+++ app/Enums/ClubCategory.php
@@ -11,7 +11,6 @@ enum ClubCategory: string
     case INTERNATIONAL = 'international';
     case TECHNOLOGY = 'technology';
 
-    // Optional: Helper method for labels
     public function label(): string
     {
         return match($this) {
