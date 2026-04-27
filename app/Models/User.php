33887c204f426b96aa7f251e84074d6fc6f8d336
loozh <loozhiheng@gmail.com>
2026-04-27 14:00:06 +0800

    fixed redundant line

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Notifications\Notifiable;
